ae4ab9bde0aa364b0159aecaa7f6d50494cdb25c
saiful-islam265 <saifulislam265@gmail.com>
2023-05-30 19:04:16 +0600

    Add latest article sections

--- blog/templatetags/article_tags.py
+++ blog/templatetags/article_tags.py
@@ -7,3 +7,9 @@ register = template.Library()
 def total_articles():
     return Article.publishedArticles.count()
     pass
+
+@register.inclusion_tag('blog/latest_articles.html')
+def show_latest_articles(count=3):
+    latest_articles = Article.publishedArticles.order_by('-publish')[:count]
+    return {'latest_articles': latest_articles}
+    pass
