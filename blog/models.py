081ff64c4af9ad2a01e444d22bd64e0427c17f37
saiful-islam265 <saifulislam265@gmail.com>
2023-05-30 18:32:30 +0600

    Add pagination to the article list page

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,8 @@ from django.db import models
 from django.db import models
 from django.contrib.auth.models import User
 from django.utils import timezone
+from django.urls import reverse
+
 
 
 class ArticlePublishedManager(models.Manager):
--- blog/views.py
+++ blog/views.py
@@ -1,14 +1,22 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Article
 from django.http import HttpResponse, Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 
 # Create your views here.
 def list_of_articles(request):
     articles = Article.publishedArticles.all()
-    #print(articles)
 
-    #return HttpResponse("this is list of articles webpage...")
+    paginator = Paginator(articles, 3)
+    page_number = request.GET.get('page', 1)
+    try:
+        articles = paginator.page(page_number)
+    except EmptyPage:
+        articles = paginator.page(paginator.num_pages)
+    except PageNotAnInteger:
+        articles = paginator.page(1)
+
     return render(request, 'blog/list.html', {'articles': articles})
     pass
 
